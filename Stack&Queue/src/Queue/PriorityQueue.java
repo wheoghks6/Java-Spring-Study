e8aad6035a79b695d8c1fe9f4decffb4cb3dc5b2
wheoghks6 <wheoghks0309@gmail.com>
2021-07-29 15:13:51 +0900

    Stack&Queue

---
+++ Stack&Queue/src/Queue/PriorityQueue.java
@@ -0,0 +1,21 @@
+package Queue;
+
+import java.util.Queue;
+
+public class PriorityQueue {
+    public static void main(String[] args) {
+        Queue pq = new java.util.PriorityQueue();
+        pq.offer(3);
+        pq.offer(1);
+        pq.offer(5);
+        pq.offer(2);
+        pq.offer(4);
+        System.out.println(pq);
+
+        Object obj = null;
+
+        while((obj = pq.poll()) != null)
+            System.out.println(obj);
+
+    }
+}
