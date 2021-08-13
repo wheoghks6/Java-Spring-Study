1ed81faeee0a6411e292b0d2dd86ae88e4afd281
wheoghks6 <wheoghks0309@gmail.com>
2021-08-13 22:42:54 +0900

    Thread ex

--- Thread/Thread_Priority/ThreadEx8.java
+++ Thread/Thread_Priority/ThreadEx8.java
@@ -5,7 +5,7 @@ public class ThreadEx8 {
         ThreadEx8_1 th1 = new ThreadEx8_1();
         ThreadEx8_2 th2 = new ThreadEx8_2();
 
-        th2.setPriority(9);
+        th2.setPriority(Thread.MAX_PRIORITY);
 
         System.out.println("Priority of th1(-) : "+th1.getPriority());
         System.out.println("Priority of th2(|) : "+th2.getPriority());
@@ -17,7 +17,7 @@ public class ThreadEx8 {
 class ThreadEx8_1 extends Thread{
     @Override
     public void run() {
-        for(int i=0; i<300; i++){
+        for(int i=0; i<1000; i++){
             System.out.print("-");
             for(int x=0; x < 10000000; x++);
         }
@@ -27,7 +27,7 @@ class ThreadEx8_1 extends Thread{
 class ThreadEx8_2 extends Thread{
     @Override
     public void run() {
-        for(int i=0; i<300; i++){
+        for(int i=0; i<1000; i++){
             System.out.print("|");
             for(int x=0; x<10000000;x++);
         }
