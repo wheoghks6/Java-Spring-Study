a28a8475ffee551a30f8e8bd99a74976d41fe495
wheoghks6 <wheoghks0309@gmail.com>
2021-09-10 14:53:54 +0900

    Byte Stream Ex

---
+++ ByteInputOutputStream/IOEx4.java
@@ -0,0 +1,31 @@
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class IOEx4 {
+    public static void main(String[] args) {
+        byte[] inSrc = {0,1,2,3,4,5,6,7,8,9};
+        byte[] outSrc = null;
+        byte[] temp = new byte[4];
+
+        ByteArrayInputStream input = new ByteArrayInputStream(inSrc);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        try{
+            while(input.available() > 0){
+                int len = input.read(temp);
+                output.write(temp, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        outSrc = output.toByteArray();
+
+        System.out.println("Input Source :"+ Arrays.toString(inSrc));
+        System.out.println("temp : "+Arrays.toString(temp));
+        System.out.println("Output Source : "+Arrays.toString(outSrc));
+
+    }
+}
